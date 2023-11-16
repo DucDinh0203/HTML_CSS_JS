dc8895daa87fa909f7d27b38ec3e2060e24c3792
Duc Dinh <fxdinhtriduc@gmail.com>
2023-11-17 02:32:27 +1100

    adding negative sign, dot for decimal number and creating setOperator function

--- calculator/calculating.js
+++ calculator/calculating.js
@@ -4,6 +4,7 @@ let previousOperator;
 screen = document.querySelector('.screen');
 
 function buttonClick(value){
+  // if value is not a number
   if(isNaN(parseInt(value))) {
     handleSymbol(value);
   } else {
@@ -15,7 +16,19 @@ function buttonClick(value){
 function handleNumber(value){
   if(input === '0') {
     input = value;
-  } else {
+  } 
+  else if (input.length == 20) {
+    return alert("Your input is too long");
+  } 
+  else if (input.includes('-0')) {
+    if(value === '0'){
+      return;
+    }
+    else {
+      input = input.substring(0, input.length-1) + value;
+    }
+  }
+  else {
     input += value;
   }
   // console.log(input);
@@ -44,11 +57,58 @@ function flushOperation(intInput){
   } else if (previousOperator === '-') {
     runningTotal -= intInput;
   } else if (previousOperator === '×') {
-    runningTotal -= intInput;
+    runningTotal *= intInput;
   } else {
     runningTotal /= intInput;
   }
   console.log(previousOperator);
+}
+
+function setOperation(operation){
+  var elements = document.getElementsByClassName("operator");
+
+  for(var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor  = "#e68a00";
+  }
+
+  if (operation === '+') {
+    document.getElementById("plusOp").style.backgroundColor = "#ffd11a";
+  } else if (operation === '-') {
+    document.getElementById("subOp").style.backgroundColor = "#ffd11a";
+  } else if (operation === '×') {
+    document.getElementById("multiOp").style.backgroundColor = "#ffd11a";
+  } else if (operation === '÷') {
+    document.getElementById("divOp").style.backgroundColor = "#ffd11a";
+  }
+}
+
+function backSpace(){
+  if (input.length === 1){
+    input = '0';
+  } else {
+    input = input.substring(0, input.length-1);
+  }
+}
+
+function decimalValue(symbol){
+  if (input.length == 1 && input === '0') {
+    input += symbol;
+  } else if (input.includes(symbol) && symbol === symbol) {
+    return;
+  } else {
+    input += symbol;
+  }
+}
+
+function plus_minus(key){ 
+  // remove negative sign when hitting "+/-" twice
+  if (input.includes(key) && key === key) {
+    input = input.slice(1);
+  } 
+  // adding negative sign
+  else {
+    input = key + input;
+  }
 }
 
 function handleSymbol(symbol){
@@ -67,25 +127,29 @@ function handleSymbol(symbol){
       input = "" + runningTotal;
       runningTotal = 0;
       break;
+    case '←':
+      backSpace();
+      break;
     case '%':
-      console.log("delete");
+      console.log("percentage");
       break;
     case '+/-':
-      console.log('negative');
+      plus_minus('-');
       break;
     case '+':
     case '-':
     case '×':
     case '÷':
-      handleMath(symbol);
+      // handleMath(symbol);
+      setOperation(symbol);
       break;
     case '.':
-      console.log('decimal');
+      decimalValue(symbol);
       break;
   }
 }
 
-
+// every "click" actions are processed by this function
 function init() {
   document
     .querySelector(".calc-buttons")
